541a7227b92e02f49524af72d089c9fe4ea39153
Julia Berglund <jullano9@hotmail.com>
2024-11-18 07:38:06 +0100

    qu1

--- övningar2/question1.js
+++ övningar2/question1.js
@@ -2,11 +2,19 @@
 // som sparas i variabeln myWord.
 let myWord = "";
 const words = ["programming", "is", "fun"];
-
+words.forEach((word) => {
+  myWord += word;
+});
 console.log(myWord); // Ska logga: "programmingisfun";
 
 // 1.2 (2p) Använd en for-lop för att lägga ihop orden i words-arrayen till en mening
 // med mellanslag mella varje ord.
 let sentence = "";
+for (let index = 0; index < words.length; index++) {
+  sentence += words[index];
+  if (index < words.length - 1) {
+    sentence += " "; // Add a space between words, except after the last one
+  }
+}
 
 console.log(sentence); // Ska logga "programming is fun";
